bd6c3ae67debab4d453418b9f4c0aa64531852c2
Konstantin Smirnov <23383424+K3130@users.noreply.github.com>
2024-03-28 12:08:02 +0300

    add base class
    add base class

---
+++ guiwrapper/guiwrapper.cpp
@@ -0,0 +1,6 @@
+#include "guiwrapper.h"
+
+GuiWrapper::GuiWrapper()
+{
+
+}
---
+++ guiwrapper/guiwrapper.h
@@ -0,0 +1,11 @@
+#ifndef GUIWRAPPER_H
+#define GUIWRAPPER_H
+
+
+class GuiWrapper
+{
+public:
+    GuiWrapper();
+};
+
+#endif // GUIWRAPPER_H
