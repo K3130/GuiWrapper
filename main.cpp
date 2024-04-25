1d513f7f6718425ce4824daa87ceca7463640f57
Konstantin Smirnov <23383424+K3130@users.noreply.github.com>
2024-04-25 12:45:25 +0300

    save settings

--- main.cpp
+++ main.cpp
@@ -12,8 +12,7 @@ int main(int argc, char** argv) {
     bool f_open = true;
     Frame f(w.GetWindowPoint(), 0, 0, 800, 800, u8"Фрейм", &f_open,
                   ImGuiWindowFlags_None
-                | ImGuiWindowFlags_NoMove
-                | ImGuiWindowFlags_NoSavedSettings);
+                | ImGuiWindowFlags_NoMove);
     f.SetButton(500, 500, 100, 40, u8"кнопка", testFunc);
     f.SetText(100, 100, u8"Обычный текст.");
     w.SetFrame(f);
